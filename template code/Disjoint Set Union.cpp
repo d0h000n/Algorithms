9befce81e6c619a5bf8cf0577762260101b8bc4f
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-01-22 01:34:53 +0900

    Update Disjoint Set Union.cpp

--- template code/Disjoint Set Union.cpp
+++ template code/Disjoint Set Union.cpp
@@ -4,27 +4,28 @@ using namespace std;
 // (큰 집합으로 병합) + (경로 압축)
 struct DSU {
   int N;
-  //follow: 집합의 크기
-  //link: 집합의 대푯값
-  int follow[N], link[N];
-  
+  //size: 집합의 크기
+  //head: 집합의 대푯값
+  int follow[N], head[N];
+  //초기화
   void init() {
-    fill_n(follow, N, 1);
-    for (int i = 1; i < N; ++i) link[i] = i;
+    fill_n(size, N, 1);
+    for (int i = 1; i < N; ++i) head[i] = i;
     return;
   }
   //union 연산
+  //same(a, b) == false여야 호출한다
   void merge(int a, int b) {
     a = find(a); b = find(b);
-    if (follow[a] < follow[b]) swap(a, b);
-    follow[a] += follow[b];
-    link[b] = a;
+    if (size[a] < size[b]) swap(a, b);
+    size[a] += follow[b];
+    head[b] = a;
     return;
   }
   //find 연산
   int find(int x) {
-    if (x == link[x]) return x;
-    return link[x] = find(link[x]);
+    if (x == head[x]) return x;
+    return head[x] = find(head[x]);
   }
   //same 연산
   inline int same(int a, int b) return find(a) == find(b);
