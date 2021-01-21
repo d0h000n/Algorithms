04bdecf1e583836b1870b91953f617fe6a71a793
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-01-22 00:29:30 +0900

    Update Disjoint Set Union.cpp

--- problemsolving/Disjoint Set Union.cpp
+++ problemsolving/Disjoint Set Union.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <memory>
 using namespace std;
 
 // (큰 집합으로 병합) + (경로 압축)
@@ -7,12 +8,19 @@ struct disjointSetUnion {
   //follow: 집합의 크기
   //link: 집합의 대푯값
   int follow[N], link[N];
+  
+  void init() {
+    fill_n(follow, N, 1);
+    for (int i = 1; i < N; ++i) link[i] = i;
+    return;
+  }
   //union 연산
   void merge(int a, int b) {
     a = find(a); b = find(b);
     if (follow[a] < follow[b]) swap(a, b);
     follow[a] += follow[b];
     link[b] = a;
+    return;
   }
   //find 연산
   int find(int x) {
