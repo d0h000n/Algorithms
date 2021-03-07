33da72d9dfdbe33302c8890f494a53818c92927d
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-03-07 18:13:58 +0900

    Update and rename modint.cpp to mod prime combination.cpp

---
+++ mod prime combination.cpp
@@ -0,0 +1,20 @@
+#define REP(i,a,b) for (int i = a; i <= b; ++i)
+#define PERh(i,front,back) for (int i = back-1; i >= front; --i)
+using ll = long long;
+
+const int N = 1e6+3, P = 1e9+7;
+ll inv[N], fac[N], ifac[N];
+
+#define positiveCombination(x,y) (C(x,y)+P)%P
+
+//Can be negative number.
+inline ll C(int x, int y) {return (x < y ? 0 : (((ll(fac[x])*ifac[y])%P)*ifac[x-y])%P);}
+
+//precompute [1,n]'s inverses and factorial inverses
+void precomputeCombinationInverses(int n) {
+  inv[1] = 1; fac[0] = 1; ifac[n] = 1; ifac[0] = 1;
+  REP(i,2,n) inv[i] = ((ll)        -P/i*inv[P%i]        )%P;
+  REP(i,1,n) fac[i] = ((ll)        fac[i-1]*i           )%P;
+  REP(i,1,n) ifac[n] = ((ll)       ifac[n]*inv[i]       )%P;
+  PERh(i,1,n) ifac[i] = (ll        (i+1)*ifac[i+1]      )%P;
+}
