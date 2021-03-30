45efa7b515ba88a0e77000f29b4a312f9e95f729
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-03-31 00:09:37 +0900

    Update mod prime combination.cpp

--- template code/mod prime combination.cpp
+++ template code/mod prime combination.cpp
@@ -1,5 +1,5 @@
 #define REP(i,a,b) for (int i = a; i <= b; ++i)
-#define PERh(i,front,back) for (int i = back-1; i >= front; --i)
+#define PER(i,a,b) for (int i = b; i >= a; --i)
 using ll = long long;
 
 const int N = 1e6+3, P = 1e9+7;
@@ -13,8 +13,8 @@ inline ll C(int x, int y) {return (x < y ? 0 : (((ll(fac[x])*ifac[y])%P)*ifac[x-
 //precompute [1,n]'s inverses and factorial inverses
 void precomputeCombinationInverses(int n) {
   inv[1] = 1; fac[0] = 1; ifac[n] = 1; ifac[0] = 1;
-  REP(i,2,n) inv[i] = ((ll)        -P/i*inv[P%i]        )%P;
-  REP(i,1,n) fac[i] = ((ll)        fac[i-1]*i           )%P;
-  REP(i,1,n) ifac[n] = ((ll)       ifac[n]*inv[i]       )%P;
-  PERh(i,1,n) ifac[i] = (ll        (i+1)*ifac[i+1]      )%P;
+  REP(i,2,n) inv[i] = ((ll)     -P/i*inv[P%i]      )%P;
+  REP(i,1,n) fac[i] = ((ll)     fac[i-1]*i         )%P;
+  REP(i,1,n) ifac[n] = ((ll)    ifac[n]*inv[i]     )%P;
+  PER(i,1,n-1) ifac[i] = (ll    (i+1)*ifac[i+1]    )%P;
 }
