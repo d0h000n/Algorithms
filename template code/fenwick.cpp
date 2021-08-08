d35a6e738caebbe424fd32f27fa06d1b95eaf5ff
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-08-08 23:12:13 +0900

    Update fenwick.cpp

--- template code/fenwick.cpp
+++ template code/fenwick.cpp
@@ -4,4 +4,3 @@ struct FenwickTree {
     T sum(int k) {T s = 0; while (k) s += tree[k], k -= k&-k; return s;}
     void update(int k, T x) {while (k < N) tree[k] += x, k += k&-k;}
 };
-//펜윅트리는 구간합 빨리 구해야 할 때 빠르게 타이핑해서 활용할 수 있는 자료구조이다.
