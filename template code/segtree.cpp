593f62197a80284f1b5755cad297b27b615cd875
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-08-28 16:56:31 +0900

    Update segtree.cpp

--- template code/segtree.cpp
+++ template code/segtree.cpp
@@ -2,14 +2,13 @@ template <typename T>
 struct SegmentTree {
 private:
     int N; vector<T> tree; function<T(const T&,const T&)> op;
-    #define U(x,y) x = op(x,y)
-    #define L (k<<1)
-    #define R ((k<<1)|1)
+    #define L k<<1
+    #define R (k<<1)|1
     #define left L,s,(s+e)>>1
     #define right R,((s+e)>>1)+1,e
     void update(int t, T x, int k, int s, int e) {
         if (t < s or e < t) return;
-        if (t <= s and e <= t) {U(tree[k],x); return;}
+        if (t <= s and e <= t) {tree[k] = op(tree[k],x); return;}
         update(t,x,left), update(t,x,right);
         tree[k] = op(tree[L],tree[R]);
     }
@@ -18,7 +17,6 @@ private:
         if (a <= s and e <= b) return tree[k];
         return op(query(a,b,left),query(a,b,right));
     }
-    #undef U
     #undef L
     #undef R
     #undef left
