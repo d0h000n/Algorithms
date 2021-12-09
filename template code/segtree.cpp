b700f39644202bfbccfe1e7cf948935c751248ff
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-12-09 19:56:09 +0900

    Update segtree.cpp

--- template code/segtree.cpp
+++ template code/segtree.cpp
@@ -1,29 +1,18 @@
-template <typename T>
+template <typename T> using Operation = function<T(const T&,const T&)>;
+
+template <typename T, const int N>
 struct SegmentTree {
-private:
-    int N; vector<T> tree; function<T(const T&,const T&)> op;
-    #define L k<<1
-    #define R (k<<1)|1
-    #define left L,s,(s+e)>>1
-    #define right R,((s+e)>>1)+1,e
-    void update(int t, T x, int k, int s, int e) {
-        if (s == e) {tree[k] = x; return;}
-        if (t < s or e < t) return;
-        update(t,x,left), update(t,x,right);
-        tree[k] = op(tree[L],tree[R]);
+    vector<T> t; Operation<T> op;
+    SegmentTree(T e, Operation<T> f): t((N+2)<<2,e), op(f) {}
+    void update(int p, T x, int k = 1, int s = 0, int e = N) {
+        if (p <= s and e <= p) {t[k] = x; return;}
+        if (p < s or e < p) return;
+        update(p,x,2*k,s,(s+e)/2), update(p,x,2*k+1,(s+e)/2+1,e);
+        t[k] = op(t[2*k],t[2*k+1]);
     }
-    T query(int a, int b, int k, int s, int e) {
-        if (a <= s and e <= b) return tree[k];
-        if (b < s or e < a) return tree[0];
-        return op(query(a,b,left),query(a,b,right));
+    T query(int a, int b, int k = 1, int s = 0, int e = N) {
+        if (a <= s and e <= b) return t[k];
+        if (b < s or e < a) return t[0];
+        return op(query(a,b,2*k,s,(s+e)/2),query(a,b,2*k+1,(s+e)/2+1,e));
     }
-    #undef L
-    #undef R
-    #undef left
-    #undef right
-public:
-    SegmentTree(int n, T id, function<T(const T&,const T&)> f):
-        N(n), tree((n+1)<<2,id), op(f) {}
-    void update(int t, T x) {update(t,x,1,1,N);}
-    T query(int a, int b) {return query(a,b,1,1,N);}
 };
