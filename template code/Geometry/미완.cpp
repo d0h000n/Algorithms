32170e1e2ebff22609dbd7616fb2d6f42aeda936
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2022-01-29 03:49:07 +0900

    Update 미완.cpp

--- template code/Geometry/미완.cpp
+++ template code/Geometry/미완.cpp
@@ -6,27 +6,28 @@ namespace Geometry {
     #define cross(u,v) (conj(u)*(v)).Y
     #define ccw(a,b,c) cross((b)-(a),(c)-(b))
     
-    template <typename T> istream &operator >> (istream &is, complex<T> &p) {T x, y; is >> x >> y; p = {x,y}; return is;}
+    constexpr ld PI = acos(-1.0L), TAU = 2*PI;
+    
+    template <typename P> istream &operator >> (istream &is, complex<P> &p) {P x, y; is >> x >> y; p = {x,y}; return is;}
     
     namespace Z_2D { // 정수 2차원 좌표계
         using P = complex<ll>;    
+        #define norm(u) dot(u,u)
+        ld abs(P u) {return sqrt(ld(norm(u)));} // 다른 abs와 중복 방지.
+        ld arg(P u) {return atan2<ld,ld>(u.Y,u.X);}
+        
         auto by_xy = [](P u, P v) {return u.X == v.X ? u.Y < v.Y : u.X < v.X;};
         
         struct Seg {
             P a, b;
+            bool operator == (Seg l) {
+                return a == l.a and b == l.b;
+            }
         };
         
-        using Poly = vector<P>;
-        pair<Poly,Poly> convex_hull(Poly &p) { // p는 정렬되어 있어야 한다.
-            Poly up, lo;
-            static auto push = [](Poly &h, P x) {
-                for (int n = size(h); n >= 2 and ccw(h[n-2],h[n-1],x) >= 0; --n, h.pop_back());
-                h.push_back(x);
-            };
-            for (int i = 0, n = size(p); i < n; ++i) push(up,p[i]), push(lo,p[n-i-1]);
-            up.pop_back(), lo.pop_back();
-            return MP(up,lo);
-        }
+        struct Line {
+            P a, b;
+        };
     }
     
     namespace Taxi {
@@ -39,14 +40,24 @@ namespace Geometry {
     
     namespace R_2D {
         using P = complex<ld>;
-        const ld EPS = 1e-9, PI = acos(-1.0L);
+        constexpr ld EPS = 1e-9;
         
         bool operator == (P &u, P &v) {return abs(u.X-v.X) < EPS and abs(u.Y-v.Y) < EPS;}
         bool operator != (P &u, P &v) {return !(u == v);}
-        bool operator 
         
-        struct Seg {P A, B; // A+(B-A)k, 0<=k<=1
-            friend ld length(const Seg& s) {return abs(s.A-s.B);}
+        struct Seg {
+            P a, b;
         };
+    }
+    template <typename P, typename Poly>
+    pair<Poly,Poly> convex_hull(const vector<P> &p) { // p는 정렬되어 있어야 한다.
+        Poly up, lo;
+        static auto push = [](Poly &h, P x) {
+            for (int n = size(h); n >= 2 and ccw(h[n-2],h[n-1],x) >= 0; --n, h.pop_back());
+            h.push_back(x);
+        };
+        for (int i = 0, n = size(p); i < n; ++i) push(up,p[i]), push(lo,p[n-i-1]);
+        up.pop_back(), lo.pop_back();
+        return MP(up,lo);
     }
 }
